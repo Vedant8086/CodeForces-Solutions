794cce9dfac8eeb0c9d13bd3c6e01c68bc2916f5
Vedant Kulkarni <122975331+Vedant8086@users.noreply.github.com>
2025-06-09 22:59:17 +0530

    Add files via upload

--- 432A_Choosing_Team.cpp
+++ 432A_Choosing_Team.cpp
@@ -19,9 +19,16 @@ int main() {
     int n,x;
     cin >> n >> x;
     vector<int> v(n,0);
+    int count = 0;
     for(int i = 0;i < n;i++) {
         cin >> v[i];
+        if(v[i] < 5) {
+            if(5 - v[i]>= x) {
+                count++;
+            }
+        }
     }
-    
+    if(count >= 3) cout << count / 3 << endl;
+    else cout << 0 << endl;
     return 0;
 }
